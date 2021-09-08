1e72e86c4dbb172e7e2ea0e688c9da8ebd227d94
진우태 <79964362+Wjkdj@users.noreply.github.com>
2021-09-08 17:13:32 +0900

    Add files via upload

---
+++ aa19-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what) {
+    console.log("Hello " + what + " !");
+}
+
+hello("aa19");
+hello("redwoods, 진우태")
---
+++ aa19-rpt02/start/index_aa19.js
@@ -0,0 +1,5 @@
+var myinfo = require('./aa19info');
+
+myinfo("aa19", "Redwoods", "010-1234-5678");
+
+myinfo("aa19", "Comsi", "010-5678-1234");
